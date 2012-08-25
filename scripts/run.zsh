864e8f7ff7c21a37862be6e5cad04d14a4facc06
Jack Dubie <jack@orchestra.com>
2012-08-25 12:07:36 -0700

    Added forever to run script

--- scripts/run.zsh
+++ scripts/run.zsh
@@ -5,20 +5,20 @@ case "$1" in PROD)
   ENV='PROD' \
   NODE_PATH=`pwd` \
   DEBUG='replicant*' \
-    ./node_modules/.bin/coffee app.coffee;
+    ./node_modules/.bin/forever -c ./node_modules/.bin/coffee app.coffee; 
   ;;
 STAGE)
   echo 'Running STAGING';
   ENV='STAGE' \
   NODE_PATH=`pwd` \
   DEBUG='replicant*' \
-    ./node_modules/.bin/coffee app.coffee;
+    ./node_modules/.bin/forever -c ./node_modules/.bin/coffee app.coffee; 
   ;;
 *)
   echo "Running DEVELOP";
   ENV='DEV' \
   NODE_PATH=`pwd` \
   DEBUG='replicant*' \
-    ./node_modules/.bin/coffee app.coffee;
+    ./node_modules/.bin/forever -c ./node_modules/.bin/coffee app.coffee; 
   ;;
   esac
